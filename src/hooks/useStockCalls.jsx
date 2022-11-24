f3d68a45748e0e921a376584ca142bd522b0a517
muharrem gem <muharremgem28@gmail.com>
2022-11-24 17:58:46 +0300

    commit

--- src/hooks/useStockCalls.jsx
+++ src/hooks/useStockCalls.jsx
@@ -1,9 +1,11 @@
 import { axiosWithToken } from "../service/axiosInstance";
 import { useDispatch } from "react-redux";
 import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useAxios from "./useAxios";
 
 const useStockCalls = () => {
   const dispatch = useDispatch();
+  const { axiosWithToken } = useAxios();
 
   const getStockData = async (url) => {
     dispatch(fetchStart());
@@ -19,7 +21,6 @@ const useStockCalls = () => {
 
   const getFirms = async () => getStockData("firms");
   const getSales = async () => getStockData("sales");
-
 
   return { getFirms, getSales };
 };
