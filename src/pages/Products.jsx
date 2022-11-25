c89d407507665317db6f342fd330ad01c3df9b77
muharrem gem <muharremgem28@gmail.com>
2022-11-25 12:01:31 +0300

    products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,19 @@
 import { Typography, Box, Button, Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import FirmCard from "../components/FirmCard";
-import FirmModal from "../components/modals/FirmModal";
 import useStockCalls from "../hooks/useStockCalls";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Firms = () => {
   const { getBrands, getCategories, getProducts } = useStockCalls();
-  const { firms } = useSelector((state) => state.stock);
+  const { products } = useSelector((state) => state.stock);
   const [open, setOpen] = useState(false);
   const [info, setInfo] = useState({});
 
@@ -26,16 +32,38 @@ const Firms = () => {
       <Button variant="contained" onClick={() => setOpen(true)}>
         New Products
       </Button>
-
       {/* <ProductModal open={open} setOpen={setOpen} info={info} setInfo={setInfo} /> */}
-      {firms?.length > 0 && (
-        <Grid container justifyContent="center" gap={3}>
-          {firms?.map((firm) => (
-            <Grid item key={firm?.id}>
-              <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
-            </Grid>
-          ))}
-        </Grid>
+
+      {products?.length > 0 && (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </Box>
   );
