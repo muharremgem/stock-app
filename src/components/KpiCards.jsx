090e0dd868816c695ff90f9556e0f4b3978d661c
muharrem gem <muharremgem28@gmail.com>
2022-11-28 16:34:21 +0300

    avatar

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,7 +1,52 @@
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PaymentsIcon from "@mui/icons-material/Payments";
+import { amber, indigo, pink } from "@mui/material/colors";
 
 const KpiCards = () => {
-  return <div>KpiCards</div>;
+  const data = [
+    {
+      title: "Sales",
+      metric: "$0",
+      icon: <MonetizationOnIcon />,
+      color: indigo[900],
+      bgColor: indigo[300],
+    },
+    {
+      title: "Profit",
+      metric: "$0",
+      icon: <PaymentsIcon />,
+      color: pink[900],
+      bgColor: pink[300],
+    },
+    {
+      title: "Purchase",
+      metric: "$0",
+      icon: <ShoppingCartIcon />,
+      color: amber[900],
+      bgColor: amber[300],
+    },
+  ];
+  return (
+    <Grid container>
+      {data.map((item) => (
+        <Grid item>
+          <Paper>
+            <Box>
+              <Avatar></Avatar>
+              <Box>
+                <Typography></Typography>
+                <Typography></Typography>
+              </Box>
+            </Box>
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default KpiCards;
