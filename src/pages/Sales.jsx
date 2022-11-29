864aad5c8b3e2fd875333b4ad5154677b236b626
muharrem gem <muharremgem28@gmail.com>
2022-11-29 21:25:06 +0300

    sales

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,5 +1,13 @@
 const Sales = () => {
-  return <div>Sales</div>;
+  return <div>import React from 'react'
+  
+  const Sales = () => {
+    return (
+      <div>Sales</div>
+    )
+  }
+  
+  export default Sales</div>;
 };
 
 export default Sales;
